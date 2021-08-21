55571499782fb8005d83b81694d8eb4f6067e4ef
Akatsuki96 <marco.rando0396@gmail.com>
2021-08-21 13:00:41 +0200

    updated safe adabkb

--- adabkb/optimizer/safe_adabkb.py
+++ adabkb/optimizer/safe_adabkb.py
@@ -29,8 +29,8 @@ class SafeAdaBKB(AdaBKB):
         self.leaf_set = np.delete(self.leaf_set, unsafe_indices, 0)
         assert len(self.leaf_set) <= old_leaf_set_size
 
-    def initialize(self, target_fun, search_space, N : int = 2, budget : int = 1000, h_max: int = 10):
-        super().initialize(target_fun, search_space, N, budget, h_max)
+    def initialize(self, search_space, N : int = 2, h_max: int = 10):
+        super().initialize(search_space, N, h_max)
         #self._remove_unsafe_partitions_()
         self._evaluable_centroid_()
 
@@ -111,32 +111,21 @@ class SafeAdaBKB(AdaBKB):
                 self.current_best = (self.leaf_set[leaf_idx], avg_rew)
 
     def step(self):
+
         while True:
+            if len(self.leaf_set)==0:
+                return self.current_best, None
             leaf_idx, Vh = self._select_node()
             node_idx =self.node2idx[tuple(self.leaf_set[leaf_idx].x)]
             self._update_best(node_idx, leaf_idx, Vh)
             if (self._centroid_accurate(node_idx, Vh) or self._unsafe_centroid(node_idx, Vh)) and self.leaf_set[leaf_idx].level <= self.h_max: # and self.leaf_set[leaf_idx].level < self.h_max:
-                self._expand(leaf_idx, node_idx)
+                print("[--] Expand")
+                self._expand(leaf_idx, node_idx, len(self.leaf_set) == 1)
                 self._evaluable_centroid_()
-            elif  self._unsafe_centroid(node_idx, Vh) and self.I[leaf_idx] >= self.j_min: #self._centroid_accurate(node_idx, Vh) and
-                print("[--] unsafe centroid on safe partition")
-                print("[--] h: {}\tVh: {}".format(self.leaf_set[leaf_idx].level, Vh))
-                #self.leaf_set = np.delete(self.leaf_set, leaf_idx, 0)
-                #self.I = np.delete(self.I, leaf_idx, 0)
-                print("[--] leaf set size: {}".format(len(self.leaf_set)))
-                self._expand(leaf_idx, node_idx)
-                print("[--] bef rem => leaf set size: {}".format(len(self.leaf_set)))
-                self._remove_unsafe_partitions_()
-                print("[--] after rem => leaf set size: {}".format(len(self.leaf_set)))
-                #    dummy_y = self.means[node_idx] - self.beta*np.sqrt(self.variances[node_idx]) - Vh
-            #    if self.verbose:
-            #        print("[--] x: {}\tfake y: {}".format(self.leaf_set[leaf_idx].x, dummy_y))
-            #    self.update_model(node_idx, dummy_y)
-            #    self._evaluable_centroid_()
-            #elif self._unsafe_centroid(node_idx, Vh):
-            #    self.leaf_set = np.delete(self.leaf_set, leaf_idx, 0)
-            #    self.I = np.delete(self.I, leaf_idx, 0)  
+            elif  self._unsafe_centroid(node_idx, Vh): #self._centroid_accurate(node_idx, Vh) and
+                print("[--] removing unsafe")
+                self.leaf_set = np.delete(self.leaf_set, leaf_idx, 0)
+                self.I = np.delete(self.I, leaf_idx, 0)
                 self._evaluable_centroid_()          
             else:
-                print("[--] lcb: {}\t j_min: {}".format(self.means[node_idx] - self.beta * np.sqrt(self.variances[node_idx]), self.j_min))
                 return self.leaf_set[leaf_idx].x, node_idx
