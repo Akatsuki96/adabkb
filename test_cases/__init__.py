9fec0e7666dab111fa01c0bb9c8cfa94b2ea1466
Akatsuki96 <marco.rando0396@gmail.com>
2021-08-25 13:49:51 +0200

    removed test_cases

--- test_cases/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bf_test_cases import GenericBFTestCase, BraninBFTestCase
-from .adabkb_test_cases import AdaBKBTestCase, AdaBBKBTestCase
-
-__all__ = ('GenericBFTestCase', 'BraninBFTestCase', 'AdaBKBTestCase', 'AdaBBKBTestCase')
