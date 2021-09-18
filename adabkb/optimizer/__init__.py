bbc5fcbebe1e5cf7a350137747df0aaf6012ca38
Akatsuki96 <marco.rando0396@gmail.com>
2021-09-18 19:11:09 +0200

    Added CMABKB

--- adabkb/optimizer/__init__.py
+++ adabkb/optimizer/__init__.py
@@ -1,5 +1,6 @@
 from .opt import AbsOptimizer
 from .adabkb import AdaBKB
 from .gradbkb import GradBKB
+from .cmabkb import CMABKB
 
-__all__ = ('AbsOptimizer', 'AdaBKB', 'GradBKB')
+__all__ = ('AbsOptimizer', 'AdaBKB', 'GradBKB', 'CMABKB')
