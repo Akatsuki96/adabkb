b9d8ff7bbcbf1db281d2027090ea9f40cd1cca51
Akatsuki96 <marco.rando0396@gmail.com>
2021-09-21 08:28:03 +0000

    Modified library for AISTATS

--- adabkb/optimizer/__init__.py
+++ adabkb/optimizer/__init__.py
@@ -1,6 +1,4 @@
 from .opt import AbsOptimizer
 from .adabkb import AdaBKB
-from .gradbkb import GradBKB
-from .cmabkb import CMABKB
 
-__all__ = ('AbsOptimizer', 'AdaBKB', 'GradBKB', 'CMABKB')
+__all__ = ('AbsOptimizer', 'AdaBKB')
